09a14b082be17595fdd2baf3d3f63db7708ada8d
汤凯 <1292737216@qq.com>
2022-09-14 17:51:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dynamic Movement Primitives
+This repository contains demos of DMP, including self-defined DMP classes, test scripts and  `pbdlib` from [Calinon](https://www.idiap.ch/software/pbdlib/).  
+---  
+**Contents:**  
+- classic DMP expressed by canonical system & transformation system  
+- Training Methods include: Least Square(LS-batch), LWR(batch), Recursive Least Square(RLS), Recursive LWR(RLWR), GMR  
+
---
+++ my_util/rbf_Basis.m
@@ -0,0 +1,14 @@
+function psi = rbf_Basis(Data, Mu, Sigma)
+%rbf_Basis: multi-dimensional input RBF basis function with only 2 hyperparameters
+% Inputs -----------------------------------------------------------------
+%   Data:  D x N array representing N datapoints of D dimensions.
+%   Mu:    D x 1 vector representing the center of the Gaussian.
+%   Sigma: D x D array representing the covariance matrix of the Gaussian.
+% Output -----------------------------------------------------------------
+%   psi:  1 x N vector.
+nbData = size(Data,2);
+Data = Data - repmat(Mu,1,nbData);
+psi = sum((Sigma\Data).*Data,1);
+psi = exp(-0.5*psi);
+end
+
